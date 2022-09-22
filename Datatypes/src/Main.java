e9459dbce8a37cc6e4a392b86a0584bd14648d34
Frknkutlu <113607351+Frknkutlu@users.noreply.github.com>
2022-09-22 13:24:32 +0300

    Add files via upload

---
+++ Datatypes/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double sayi = 12.2;
+		sayi =-129;
+		
+		
+		System.out.println(sayi);
+
+		char karakter ='A';
+		boolean dogruMu=false;
+		
+		if (dogruMu==true) {
+		}	
+
+	}
+
+}
---
+++ MiniProjeAsalSayi/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+	public static void main(String[] args) {
+		//Sayıyı buraya giriniz.
+		int number= 0;
+		//int remainder= number % 2;
+		//System.out.println(remainder);
+		boolean isPrime = true;
+		
+		if(number==1) {
+			System.out.println("Sayi asal degildir.");
+			return;
+		}
+		
+		
+		if(number<1) {
+			System.out.println("Gecersiz sayi girdiniz.");
+			return;
+		}
+		
+		
+		for(int i=2; i<number; i++) {
+			if(number % i ==0) {
+				isPrime =false;
+			}
+		}
+		
+		if(isPrime) {
+			System.out.println("Sayi asaldir.");
+		}else {
+			System.out.println("Sayi asal degildir.");
+		}
+	}
+				
+}
